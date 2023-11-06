aedee74a57d09ff8ff327627e7a5e9b61b460063
hahrahr <hammedsaudix@gmail.com>
2023-11-06 08:29:14 +0200

    Add SplashActivity Screen

---
+++ app/src/main/java/com/projecttask/tasks_requested_for_graduation_year/ui/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.projecttask.tasks_requested_for_graduation_year.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projecttask.tasks_requested_for_graduation_year.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
